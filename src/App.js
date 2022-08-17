711bb1b6f7b0103212e10aebf8e028d5b1992f3f
Devi Rajeswari <devirajeswari06@gmail.com>
2022-08-17 12:19:23 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import'./Pages/Homepage/Homepage.css'
+import './App.css';
+
+import Homepage from './Pages/Homepage/Homepage';
+
+function App() {
+  return (
+    <Homepage />
+  );
+}
+
+export default App;
---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <>
+    <h1 className='heading'>Welcome to Icecream Parlor</h1>
+    <p>Life is like an ice cream: Enjoy it before it melts. </p>
+    <img className='pic' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHyySKjEK_qV7kaD4b7rPbtC8AzYzPWo_RDw&usqp=CAU" alt="img"></img>
+    </>
+  )
+}
+
+export default Homepage
